4d7a3d3f058e29f907f2d91d7d125bd3b0a2d298
Dyke <dykekangoma@gmail.com>
2024-08-18 12:31:27 +0200

    Django Views and URL Configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -13,3 +13,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+from django.shortcuts import render
+from django.views.generic.detail import DetailView  # Import DetailView here
+from .models import Book, Library
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
