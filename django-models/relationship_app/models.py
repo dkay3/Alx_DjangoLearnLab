7a1d73a7729ee55f7e9006a10a49a7b26a612c69
Dyke <dykekangoma@gmail.com>
2024-08-18 11:31:51 +0200

    Implementing Advanced Model Relationships in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,14 +3,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)  # ForeignKey relationship
+
+    def __str__(self):
+        return self.title
 
 class Publisher(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)  # ManyToMany relationship
 
+    def __str__(self):
+        return self.name
+
 class Profile(models.Model):
     user = models.OneToOneField(Author, on_delete=models.CASCADE)  # OneToOne relationship
     bio = models.TextField()
+
+    def __str__(self):
+        return self.user.name
