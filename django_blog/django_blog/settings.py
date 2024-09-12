c44475da2e448561da2838ad080a6551bd4ab36e
Dyke <dykekangoma@gmail.com>
2024-09-12 09:06:31 +0200

    Initial Setup and Project Configuration for a Django Blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
