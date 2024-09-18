8b8a3436ac7714d03114f8a89ef12eeef2d0b58d
Dyke <dykekangoma@gmail.com>
2024-09-18 10:06:20 +0200

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -72,7 +72,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)  # Use get_object_or_404
+        post = generics.get_object_or_404(Post, pk=pk)  # Use get_object_or_404
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
