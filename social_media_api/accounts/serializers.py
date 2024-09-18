6288595ec6ebccd85fd5f8a9d413f8904089e8e5
Dyke <dykekangoma@gmail.com>
2024-09-18 09:18:18 +0200

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,20 +11,22 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'bio', 'profile_picture']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # Define password field
 
     class Meta:
         model = User
         fields = ['username', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True}  # Prevent password from being read
+        }
 
     def create(self, validated_data):
+        # Use get_user_model().objects.create_user to create a user
         user = User.objects.create_user(
-        username=validated_data['username'],
-        password=validated_data['password'],
-        bio=validated_data.get('bio', ''),
-        profile_picture=validated_data.get('profile_picture', None)
-    )
-        Token.objects.create(user=user)
+            username=validated_data['username'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)  # Create a token for the user
         return user
-
