3932b56cf2bf3de000430fe1021054440ca1cd1b
Dyke <dykekangoma@gmail.com>
2024-09-18 09:46:44 +0200

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -20,17 +20,18 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# Assuming CustomUser is your user model
+CustomUser = get_user_model()
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
-            user_to_follow = User.objects.get(id=user_id)
+            user_to_follow = CustomUser.objects.get(id=user_id)
             request.user.following.add(user_to_follow)
             return Response({'status': 'now following'}, status=status.HTTP_200_OK)
-        except User.DoesNotExist:
+        except CustomUser.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class UnfollowUserView(generics.GenericAPIView):
@@ -38,16 +39,16 @@ class UnfollowUserView(generics.GenericAPIView):
 
     def post(self, request, user_id):
         try:
-            user_to_unfollow = User.objects.get(id=user_id)
+            user_to_unfollow = CustomUser.objects.get(id=user_id)
             request.user.following.remove(user_to_unfollow)
             return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
-        except User.DoesNotExist:
+        except CustomUser.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class UserListView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        users = User.objects.all()  # Get all users
+        users = CustomUser.objects.all()  # Retrieve all users
         user_data = [{'id': user.id, 'username': user.username} for user in users]
         return Response(user_data, status=status.HTTP_200_OK)
